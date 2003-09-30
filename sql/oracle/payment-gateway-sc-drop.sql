0e042a160a79aa2244843abc43606b218873f711
Mohan Pakkurti <mohan.pakkurti@gmail.com>
2003-09-30 12:10:09 +0000

    Oracle9i porting changes   - Changed procedure names from delete to del   - Changed view declaration to use actual column names   - Changed "rel_constraints_violated_one" and "rel_constraints_violated_two"     (http://openacs.org/forums/message-view?message_id=81045)

--- sql/oracle/payment-gateway-sc-drop.sql
+++ sql/oracle/payment-gateway-sc-drop.sql
@@ -1,15 +1,15 @@
 begin
-  acs_sc_contract.delete('PaymentGateway');
-  acs_sc_msg_type.delete(null,'PaymentGateway.Authorize.InputType');
-  acs_sc_msg_type.delete(null,'PaymentGateway.Authorize.OutputType');
-  acs_sc_msg_type.delete(null,'PaymentGateway.ChargeCard.InputType');
-  acs_sc_msg_type.delete(null,'PaymentGateway.ChargeCard.OutputType');
-  acs_sc_msg_type.delete(null,'PaymentGateway.Return.InputType');
-  acs_sc_msg_type.delete(null,'PaymentGateway.Return.OutputType');
-  acs_sc_msg_type.delete(null,'PaymentGateway.Void.InputType');
-  acs_sc_msg_type.delete(null,'PaymentGateway.Void.OutputType');
-  acs_sc_msg_type.delete(null,'PaymentGateway.Info.InputType');
-  acs_sc_msg_type.delete(null,'PaymentGateway.Info.OutputType');
+  acs_sc_contract.del('PaymentGateway');
+  acs_sc_msg_type.del(null,'PaymentGateway.Authorize.InputType');
+  acs_sc_msg_type.del(null,'PaymentGateway.Authorize.OutputType');
+  acs_sc_msg_type.del(null,'PaymentGateway.ChargeCard.InputType');
+  acs_sc_msg_type.del(null,'PaymentGateway.ChargeCard.OutputType');
+  acs_sc_msg_type.del(null,'PaymentGateway.Return.InputType');
+  acs_sc_msg_type.del(null,'PaymentGateway.Return.OutputType');
+  acs_sc_msg_type.del(null,'PaymentGateway.Void.InputType');
+  acs_sc_msg_type.del(null,'PaymentGateway.Void.OutputType');
+  acs_sc_msg_type.del(null,'PaymentGateway.Info.InputType');
+  acs_sc_msg_type.del(null,'PaymentGateway.Info.OutputType');
 end;
 /
 show errors
